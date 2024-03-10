bd6678d13cf6cf4d91cc7f935b99b5670d073db6
“ScarletWu” <scarletruoxianwu@gmail.com>
2024-03-10 22:20:20 +0000

    new qmd

--- scripts/download.R
+++ scripts/download.R
@@ -15,6 +15,6 @@ data_path <- "/cloud/project/data/13100392.csv"
 
 data <- read_csv(data_path) %>%
   clean_names() %>%
-  select(ref_date, sex, cause_of_death_icd_10, characteristics, value)
+  select(ref_date, sex, age_at_time_of_death, cause_of_death_icd_10, characteristics, value)
 
-write_csv(data, "/cloud/project/data/13100392.csv")
+write_csv(data, "/cloud/project/data/downloaded_data.csv")
